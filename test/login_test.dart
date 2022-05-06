71eae67b978cac67989c8adc4a05d8d8f150f52d
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-05-07 01:05:09 +0900

    25.Modelのロジックをテスト：メソッド

--- test/login_test.dart
+++ test/login_test.dart
@@ -15,8 +15,32 @@ void main() {
   });
 
   test('increase', () async {
+    target.increase();
+    expect(target.countData.count, 1);
+    expect(target.countData.countUp, 1);
+    expect(target.countData.countDown, 0);
+
+    target.increase();
+    target.increase();
+    target.increase();
+    expect(target.countData.count, 4);
+    expect(target.countData.countUp, 4);
+    expect(target.countData.countDown, 0);
+  });
+
+  test('decrease', () async {
+    target.decrease();
+    expect(target.countData.count, -1);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 1);
+
+    target.decrease();
+    target.decrease();
+    target.decrease();
+    expect(target.countData.count, -4);
+    expect(target.countData.countUp, 0);
+    expect(target.countData.countDown, 4);
 
   });
-  test('decrease', () async {});
   test('reset', () async {});
 }
