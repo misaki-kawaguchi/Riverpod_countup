687773a78368b2ab80103122a8b06d20c4024a8c
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-05-06 23:04:15 +0900

    セクション1：Riverpodの基本 (#1)
    * 4.インストール
    
    * 6.MyAppをProviderScopeで囲む
    
    * 7.Riverpodを使用するための準備
    
    * 8.タイトル用のProviderの作成
    
    * 9.メッセージ用のProviderの作成
    
    * 10.StateProviderの作成
    
    * 13.全てConsumerに書き換える
    
    * 14.なぜreadを使うか
    
    * 15.ConsumerStateWidget

---
+++ lib/provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// refを通じてriverpodを通信する
+final titleProvider = Provider<String>((ref) {
+  return  'Riverpod Demo Home Page';
+});
+
+final messageProvider = Provider<String>((ref) {
+  return 'You have pushed the button this many times:';
+});
+
+final countProvider = StateProvider<int>((ref) => 0);
