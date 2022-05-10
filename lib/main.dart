df1de3dc3be0e35cfb350febe416423229dd2b24
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2022-05-11 00:34:52 +0900

    セクション2: MVVMパターン (#2)
    * 17.MVVMように外観を書き換える
    
    * 18.freezedの解説とインストール
    
    * 19.freezedでデータクラスを作成
    
    * 20.データクラスをStateProviderにする
    
    * 22.selectでWidgetの再構築を抑える
    
    * 23.Modelのロジックのクラスを作る
    
    * 24.Modelのロジックをテスト：インストールと初期値
    
    * 25.Modelのロジックをテスト：メソッド
    
    * 26.Modelのロジックをテスト：リセット
    
    * 27.ViewModelのクラスを作る
    
    * 28.ViewModelに置き換えていく
    
    * 29.ViewModelでメソッドを置き換えていく
    
    * 31.ViewとViewModelの繋がりをWidgetTestする
    
    * 32.ViewとViewModelの繋がりをGoldenTestする準備する
    
    * 33.ViewとViewModelの繋がりをGoldenTestする
    
    * 34.GoldenTestでフォントを表示させる
    
    * 36.GoldenTestでtapしてみる
    
    * 37.GoldenTestでMockをしてみる
    
    * 38.GoldenTestでMocktailで数値をテストする
    
    * 39.GoldenTestでMocktailでタップ回数を確認する
    
    * 40.Providerの中身を入れ替える

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/data/count_data.dart';
 import 'package:riverpod_countup/provider.dart';
+import 'package:riverpod_countup/view_model.dart';
 
 void main() {
   runApp(
@@ -21,19 +24,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MyHomePage(
+        ViewModel(),
+      ),
     );
   }
 }
 
 class MyHomePage extends ConsumerStatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  final ViewModel viewModel;
+  MyHomePage(this.viewModel, {Key? key}) : super(key: key);
 
   @override
   ConsumerState<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends ConsumerState<MyHomePage> {
+  late ViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = widget.viewModel;
+    _viewModel.setRef(ref);
+  }
+
   @override
   // Riverpodのproviderにアクセスできる
   Widget build(BuildContext context) {
@@ -51,17 +66,41 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
               ref.watch(messageProvider),
             ),
             Text(
-              ref.watch(countProvider).toString(),
+              _viewModel.count,
               style: Theme.of(context).textTheme.headline4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FloatingActionButton(
+                  onPressed: _viewModel.onIncrease,
+                  child: const Icon(CupertinoIcons.plus),
+                ),
+                FloatingActionButton(
+                  onPressed: _viewModel.onDecrease,
+                  child: const Icon(CupertinoIcons.minus),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  _viewModel.countUp,
+                ),
+                Text(
+                  _viewModel.countDown,
+                ),
+              ],
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         // read：再構築されないようになる
-        onPressed: () => ref.read(countProvider.state).state++,
+        onPressed: _viewModel.onReset,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/data/count_data.dart';
 
 // refを通じてriverpodを通信する
 final titleProvider = Provider<String>((ref) {
-  return  'Riverpod Demo Home Page';
+  return 'Riverpod Demo Home Page';
 });
 
 final messageProvider = Provider<String>((ref) {
@@ -10,3 +11,9 @@ final messageProvider = Provider<String>((ref) {
 });
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>((ref) => const CountData(
+      count: 0,
+      countUp: 0,
+      countDown: 0,
+    ));
