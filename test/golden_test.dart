0d739f60552ae108c993e986c4cd5dfac2a67603
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-05-11 00:09:17 +0900

    38.GoldenTestでMocktailで数値をテストする

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -3,20 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:riverpod_countup/main.dart';
 import 'package:riverpod_countup/view_model.dart';
+
+class MockViewModel extends Mock implements ViewModel {}
 
 void main() {
   setUpAll(() async {
     await loadAppFonts();
   });
 
+  const iPhone55 =
+      Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
+  List<Device> devices = [iPhone55];
+
   testGoldens('normal', (tester) async {
-    const iPhone55 =
-        Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
-
-    List<Device> devices = [iPhone55];
-
     ViewModel viewModel = ViewModel();
     await tester.pumpWidgetBuilder(
       ProviderScope(
@@ -40,6 +42,25 @@ void main() {
     await multiScreenGolden(
       tester,
       'myHomePage_1tapped',
+      devices: devices,
+    );
+  });
+
+  testGoldens('viewModelTest', (tester) async {
+    var mock = MockViewModel();
+    when(() => mock.count).thenReturn(1123456789.toString());
+    when(() => mock.countUp).thenReturn(2123456789.toString());
+    when(() => mock.countDown).thenReturn(3123456789.toString());
+
+    await tester.pumpWidgetBuilder(
+      ProviderScope(
+        child: MyHomePage(mock),
+      ),
+    );
+
+    await multiScreenGolden(
+      tester,
+      'myHomePage_mock',
       devices: devices,
     );
   });
