7757fe4a6832cf4c6740cf4b5be31d92d9683c1e
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-05-10 23:21:30 +0900

    34.GoldenTestでフォントを表示させる

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:riverpod_countup/main.dart';
 import 'package:riverpod_countup/view_model.dart';
 
 void main() {
+  setUpAll(() async {
+    await loadAppFonts();
+  });
+
   testGoldens('normal', (tester) async {
     const iPhone55 =
         Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
